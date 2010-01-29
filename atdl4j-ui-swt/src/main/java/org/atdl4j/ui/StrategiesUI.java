99016b1cbc8b426f82b82429840fa812fe8c324e
unknown <swl@.acslan.ac.americancentury.com>
2010-01-28 23:54:27 -0600

    To support InputDataAndFilterCriteria - Modified StrategiesUI and SWTStrategiesUI’s createUI() method to accept arg of Map<String, String>

--- atdl4j-ui-swt/src/main/java/org/atdl4j/ui/StrategiesUI.java
+++ atdl4j-ui-swt/src/main/java/org/atdl4j/ui/StrategiesUI.java
@@ -1,12 +1,15 @@
 package org.atdl4j.ui;
 
+import java.util.Map;
+
 import javax.xml.bind.JAXBException;
 
-import org.atdl4j.atdl.core.StrategyT;
+import org.fixprotocol.atdl_1_1.core.StrategyT;
 
 public interface StrategiesUI<T> {
 
-	public StrategyUI createUI(StrategyT strategy, T parent)
+//TODO 1/17/2010 Scott Atwell	public StrategyUI createUI(StrategyT strategy, T parent)
+	public StrategyUI createUI(StrategyT strategy, T parent, Map<String, String> inputHiddenFieldNameValueMap)
 			throws JAXBException;
 
 }
--- atdl4j-ui-swt/src/main/java/org/atdl4j/ui/swt/impl/SWTStrategiesUI.java
+++ atdl4j-ui-swt/src/main/java/org/atdl4j/ui/swt/impl/SWTStrategiesUI.java
@@ -5,27 +5,31 @@ import java.util.Map;
 
 import javax.xml.bind.JAXBException;
 
-import org.atdl4j.atdl.core.StrategiesT;
-import org.atdl4j.atdl.core.StrategyT;
-import org.atdl4j.atdl.validation.EditT;
 import org.atdl4j.data.ValidationRule;
 import org.atdl4j.ui.StrategiesUI;
 import org.atdl4j.ui.swt.util.RuleFactory;
 import org.eclipse.swt.widgets.Composite;
+import org.fixprotocol.atdl_1_1.core.StrategyT;
+import org.fixprotocol.atdl_1_1.core.StrategiesT;
+import org.fixprotocol.atdl_1_1.validation.EditT;
 
 public class SWTStrategiesUI implements StrategiesUI<Composite> {
 
 	private Map<String, ValidationRule> strategiesRules;
+//TODO 1/16/2010 Scott Atwell added
+	private StrategiesT strategies;
 
-	public SWTStrategiesUI(StrategiesT strategies) throws JAXBException {
+	public SWTStrategiesUI(StrategiesT strategies)
+			throws JAXBException {
+//TODO 1/16/2010 Scott Atwell added
+		this.strategies = strategies;
 
 		strategiesRules = new HashMap<String, ValidationRule>();
 
 		for (EditT edit : strategies.getEdit()) {
 			String id = edit.getId();
 			if (id != null) {
-				ValidationRule rule = RuleFactory.createRule(edit,
-						strategiesRules, strategies);
+				ValidationRule rule = RuleFactory.createRule(edit, strategiesRules, strategies);
 				strategiesRules.put(id, rule);
 			} else {
 				throw new JAXBException("Strategies-scoped edit without id");
@@ -33,14 +37,17 @@ public class SWTStrategiesUI implements StrategiesUI<Composite> {
 		}
 	}
 
-	public SWTStrategyUI createUI(StrategyT strategy, Composite parent)
+//TODO 1/17/2010 Scott Atwell	public SWTStrategyUI createUI(StrategyT strategy, Composite parent)
+public SWTStrategyUI createUI(StrategyT strategy, Composite parent, Map<String, String> inputHiddenFieldNameValueMap)
 			throws JAXBException {
-		// try {
-		return new SWTStrategyUI(strategy, strategiesRules, parent);
-		// } catch (JAXBException e) {
-		// throw new JAXBException("Error in Strategy \"" + strategy.getName() +
-		// "\": " + e.getMessage());
-		// }
+		//try {
+//TODO 1/16/2010 Scott Atwell			return new SWTStrategyUI(strategy, strategiesRules, parent);
+//TODO 1/17/2010 Scott Atwell			return new SWTStrategyUI(strategy, strategiesRules, parent, strategies);
+			return new SWTStrategyUI(strategy, strategiesRules, parent, strategies, inputHiddenFieldNameValueMap);
+						
+		//} catch (JAXBException e) {
+		//	throw new JAXBException("Error in Strategy \"" + strategy.getName() + "\": " + e.getMessage());
+		//}	
 	}
 
 }
